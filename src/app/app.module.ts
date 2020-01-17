655e477c7a7480a4aafa73e1339f2e33dd9dd265
MuhammadAyub24 <43295653+MuhammadAyub24@users.noreply.github.com>
2020-01-16 20:52:30 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './Components/users/users.component';
+import { FormComponent } from './Components/form/form.component';
+import { PostsComponent } from './Components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    FormComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
